45ded6b2b40aa92c5d60f6e8f17f4c6c9afca1c1
ogomb <mbogolewis@gmail.com>
2017-07-27 07:51:33 +0300

    update unit_test.py to include more tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,9 +1,17 @@
-from app import app
+"""tests for the application."""
 import unittest
+from app import app
+from app.views import BucketList
+from app.views import User
 
 
 class BucketListApplication(unittest.TestCase):
-       
+    """test class that test many functionalities in the application. """
+
+    def setUp(self):
+        self.blist = BucketList()
+        self.my_user = User()
+
     def test_root_url_has_correct_html(self):
         """
         this function test if the homepage has the required element values in the title. """
@@ -15,24 +23,25 @@ class BucketListApplication(unittest.TestCase):
 
     def test_signup_page_redirection(self):
         """
-        this function tests if the post will redirect to another page after a post request is made. """
+        this function tests if the post will redirect to
+        another page after a post request is made. """
         client = app.test_client()
         response = client.post('/signup', data={1: "just random stuff"})
-        assert response.status == '302 FOUND'
+        self.assertEqual(response.status, '302 FOUND')
 
     def test_login_page_redirection(self):
         """
-        this function tests if the post will redirect to another page after a post request is made. """
+        this function tests if the post will redirect
+        to another page after a post request is made. """
         client = app.test_client()
         response = client.post('/login', data={'email':"some weird email here"})
-        assert response.status == '302 FOUND'
+        self.assertEqual(response.status, '302 FOUND')
 
-
-    def test_bucket_list_page_for_bucket_list(self):
+    def test_bucketlist_page_bucketlist(self):
         """
         Test if the correct post is sent."""
         client = app.test_client()
-        response = client.post('bucket_list', data={"description":["go hiking in everest"]})
+        response = client.post('bucket_list', data={"description":"go hiking in everest"})
         self.assertNotEqual('go hiking in everest', response.get_data(as_text=True))
 
 
@@ -40,10 +49,30 @@ class BucketListApplication(unittest.TestCase):
         """
         Test if the page logout works."""
         client = app.test_client()
-        return client.get('/logout', follow_redirects=True)    
-if __name__== "__main__":
+        return client.get('/logout', follow_redirects=True)
+
+    def test_bucketlist_empty_after_removingall(self):
+        """
+        Test that the bucket list contains nothing after
+        deleting everything. """
+        self.blist.delete_all_items_in_bucket()
+        self.assertEqual([], self.blist.display_list())
+
+    def test_buctetlist_has_one_item(self):
+        """test the bucket list has an item after adding one. """
+        self.blist.add_item_to_bucket("going to glide")
+        self.assertEqual(["going to glide"], self.blist.display_list())
+
+    def test_user_assignment_name(self):
+        """test that a user has a name after assigning it one. """
+        self.my_user.name = "siwel"
+        self.assertNotEqual("IMIRIK", self.my_user.name)
+
+
+    def test_the_length_ofbucketlist(self):
+        """test if the length of the bucket list is equal to the actual
+        bucket list length."""
+        self.blist = ["go to mt.Kenya", "visit longonot", "visit japan"]
+        self.assertEqual(3, len(self.blist))
+if __name__ == "__main__":
     unittest.main()
-
-
-
-
